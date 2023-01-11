6b0789257e429fa24e9aae4366f6c69cc9519bf1
Hemant Kumar <hekumar@redhat.com>
2023-01-11 14:08:41 -0500

    Add a count of zonal volumes

--- pkg/check/count_pv_types_test.go
+++ pkg/check/count_pv_types_test.go
@@ -1,9 +1,10 @@
 package check
 
 import (
+	"testing"
+
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"testing"
 )
 
 func TestCountRWXVolumes(t *testing.T) {
@@ -24,6 +25,34 @@ func TestCountRWXVolumes(t *testing.T) {
 					},
 				},
 			},
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "zonalvolume",
+				},
+				Spec: v1.PersistentVolumeSpec{
+					PersistentVolumeSource: v1.PersistentVolumeSource{
+						CSI: &v1.CSIPersistentVolumeSource{
+							Driver:       vSphereCSIDdriver,
+							VolumeHandle: "blahblah",
+						},
+					},
+					NodeAffinity: &v1.VolumeNodeAffinity{
+						Required: &v1.NodeSelector{
+							NodeSelectorTerms: []v1.NodeSelectorTerm{
+								{
+									MatchExpressions: []v1.NodeSelectorRequirement{
+										{
+											Key:      "topology.csi.vmware.com/openshift-zone",
+											Operator: v1.NodeSelectorOperator("In"),
+											Values:   []string{"us-west-1a"},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
 		},
 	}
 
@@ -34,11 +63,15 @@ func TestCountRWXVolumes(t *testing.T) {
 	defer cleanup()
 
 	// Act
-	count, err := countRWXPVsFromCluster(ctx)
+	rwxVolumeCount, zonalPVCount, err := countVolumeTypes(ctx)
 	if err != nil {
 		t.Errorf("Unexpected error : %+v", err)
 	}
-	if 1 != count {
-		t.Errorf("expected 1 rwx pv, found %d", count)
+	if 1 != rwxVolumeCount {
+		t.Errorf("expected 1 rwx pv, found %d", rwxVolumeCount)
+	}
+
+	if 1 != zonalPVCount {
+		t.Errorf("expected 1 zonal pv, found %d", zonalPVCount)
 	}
 }
