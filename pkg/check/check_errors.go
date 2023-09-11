5573c7c9b1314c9ae38e99fab3980fb7854de7bc
Hemant Kumar <hekumar@redhat.com>
2023-09-11 12:13:28 -0400

    Move all checks together

--- pkg/check/check_errors.go
+++ pkg/check/check_errors.go
@@ -15,20 +15,6 @@ const (
 	FailedGettingDatastoreCluster KnownErrorLabel = "failed_getting_datastore_cluster"
 	DatastoreClusterInUse         KnownErrorLabel = "datastore_cluster_inuse"
 
-	VcenterNotFound KnownErrorLabel = "vcenter_not_found"
-
-	NodeMissingPermissions KnownErrorLabel = "node_missing_permissions"
-	FailedGettingNode      KnownErrorLabel = "failed_getting_node"
-
-	FailedGettingHost KnownErrorLabel = "failed_getting_host"
-
-	FailedDiskLatency KnownErrorLabel = "failed_disk_latency"
-
-	EmptyNodeProviderId KnownErrorLabel = "empty_node_provider_id"
-	EmptyNodeDiskUUID   KnownErrorLabel = "empty_node_disk_uuid"
-
-	ResourcePoolMissingPermissions KnownErrorLabel = "resource_pool_missing_permissions"
-
 	MissingTaskPermissions KnownErrorLabel = "missing_task_permissions"
 
 	FailedGettingFolder      KnownErrorLabel = "failed_getting_folder"
@@ -41,8 +27,19 @@ const (
 	InvalidComputerClusterPath  KnownErrorLabel = "invalid_compute_cluster_path"
 	FailedGettingComputeCluster KnownErrorLabel = "failed_getting_compute_cluster"
 
+	// emitted by nodes and cluster checks both
+	VcenterNotFound KnownErrorLabel = "vcenter_not_found"
 	// Openshift API error labels
 	OpenshiftAPIError KnownErrorLabel = "openshift_api_error"
+
+	// emitted by only node checks
+	NodeMissingPermissions         KnownErrorLabel = "node_missing_permissions"
+	FailedGettingNode              KnownErrorLabel = "failed_getting_node"
+	FailedGettingHost              KnownErrorLabel = "failed_getting_host"
+	FailedDiskLatency              KnownErrorLabel = "failed_disk_latency"
+	EmptyNodeProviderId            KnownErrorLabel = "empty_node_provider_id"
+	EmptyNodeDiskUUID              KnownErrorLabel = "empty_node_disk_uuid"
+	ResourcePoolMissingPermissions KnownErrorLabel = "resource_pool_missing_permissions"
 
 	// we don't actually emit this error in code except for test
 	MiscError KnownErrorLabel = "misc_error"
